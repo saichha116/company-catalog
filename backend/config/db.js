f77bee8ea1c2eeb2e3d418fc15b00c99e2dfb858
saichha116 <naikmulesaichha@gmail.com>
2026-07-23 15:20:13 +0530

    Added backend structure

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "sbnma11@asdfg",
+    database: "company_catalog"
+});
+
+
+db.connect((err) => {
+    if(err){
+        console.log("Database connection failed");
+        console.log(err);
+    }
+    else{
+        console.log("MySQL Connected Successfully");
+    }
+});
+
+
+module.exports = db;
